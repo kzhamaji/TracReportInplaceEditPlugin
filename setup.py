5f80c0dd974fe3543ef262ac564afcfefc39f38d
Masakazu HAMAJI <kzhamaji@gmail.com>
2016-02-18 13:42:41 +0900

    Fork from r14057 of https://trac-hacks.org/browser/tracreportinplaceeditplugin/0.11.

---
+++ setup.py
@@ -0,0 +1,45 @@
+import sys
+from setuptools import find_packages, setup
+
+extra = {}
+
+try:
+    import babel
+    extractors = [
+        ('**.py',                'python', None),
+        ('**/templates/**.html', 'genshi', None),
+        ('**/templates/**.js',   'javascript', None),
+        ('**/templates/**.txt',  'genshi',
+         {'template_class': 'genshi.template:NewTextTemplate'}),
+    ]
+    extra['message_extractors'] = {
+        'ripe': extractors,
+    }
+except ImportError:
+    pass
+
+setup(
+    name = 'TracReportInplaceEditPlugin',
+    version = "0.2",
+    description = "Edit tickets in reports by inplace editor",
+    author = "Richard Liao",
+    author_email = "richard.liao.i@gmail.com",
+    url = "http://trac-hacks.org/wiki/TracReportInplaceEditPlugin",
+    license = "BSD",
+    packages = find_packages(exclude=['ez_setup', 'examples', 'tests*']),
+    include_package_data = True,
+    package_data = { 'ripe': [ '*.txt', 'templates/*.*', 'htdocs/*.*', 
+        'tests/*.*', 'locale/*.*', 'locale/*/LC_MESSAGES/*.*' ] },
+    zip_safe = False,
+    keywords = "trac TracReportInplaceEditPlugin",
+    classifiers = [
+        'Framework :: Trac',
+    ],
+    install_requires = ['simplejson'] if sys.version_info < (2,6) else [],
+    entry_points = """
+    [trac.plugins]
+    ripe = ripe.web_ui
+    """,
+    **extra
+    )
+
